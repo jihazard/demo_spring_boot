13ca6047c37d815dc0bd428d2ab0da8f8f5c21fd
zyoojih <jhyun@j2stechno.com>
2022-06-29 14:59:38 +0900

    pull test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
     public class TestController {
 
         @GetMapping("/test")
-        public String test() { return "test";}
+        public String test() {
+            int result = 0;
+            for (int i = 0; i < 100; i++) {
+                System.out.println(i);
+                result += i;
+            }
+            System.out.println(result);
+            return "test";
+
+        }
     }
 
 
